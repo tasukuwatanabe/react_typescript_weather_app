0a371ef98fa229859267972f945d349684f7ff9d
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2022-07-07 06:48:10 +0900

    move functions from Form.tsx to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import Title from './components/Title';
 import Form from './components/Form';
 import Results from './components/Results';
 import './App.css';
 
 function App() {
+  const [city, setCity] = useState<string>('');
+
+  const getWeather = (e: any) => {
+    e.preventDefault();
+
+    fetch(
+      'http://api.weatherapi.com/v1/current.json?key=d1c617a2c8634304a76124411220607&q=London&aqi=no'
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
   return (
     <div className="App">
       <Title />
-      <Form />
+      <Form setCity={setCity} getWeather={getWeather} />
       <Results />
     </div>
   );
